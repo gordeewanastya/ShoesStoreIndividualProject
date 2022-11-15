abf4c0e187ada693318dfbf06459e0ed5bd3cf11
Nastya Gordeeva <nastyagordeeva72@gmail.com>
2022-11-15 19:53:14 +0200

    Initial Configuration of Project

---
+++ ShoesStore/src/main/java/com/gordeeva/courses/ShoesStore/ShoesStoreProjectApplication.java
@@ -0,0 +1,13 @@
+package com.gordeeva.courses.ShoesStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoesStoreProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoesStoreProjectApplication.class, args);
+	}
+
+}
---
+++ ShoesStore/src/test/java/com/gordeeva/courses/ShoesStore/ShoesStoreProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gordeeva.courses.ShoesStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoesStoreProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
